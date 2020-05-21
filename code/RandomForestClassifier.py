a4e333e1d33ebbc661e1636d3ad544e995b2a3fc
Harris Logic <>
2020-05-21 17:47:04 +0800

    Update RandomForestClassifier.py

--- code/RandomForestClassifier.py
+++ code/RandomForestClassifier.py
@@ -23,6 +23,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.30)
 
 from sklearn.ensemble import RandomForestClassifier
 clf = RandomForestClassifier()
+
 clf.fit(X_train,y_train)
 y_pred = clf.predict(X_test)
 
